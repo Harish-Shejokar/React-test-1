245e314d8bcb41684936b4dfbac4cdd72c2bfa40
Harish-Shejokar <hshejokar@gmail.com>
2023-03-20 08:40:47 +0530

    react-test-1 completed

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React,{useState} from "react";
+import Form from "./Components/Form/Form";
+import List from "./List/List";
+
+function App() {
+
+  const [data, setData] = useState([
+    { id: '1', sp: "300", pn: "chips", cat: "food" },
+    { id: '2', sp: "600", pn: "battery", cat: "Electronics" },
+  ]);
+
+  
+
+  const addDataHandler = (enteredData) => {
+
+    setData(prevdata => {
+      return [enteredData, ...prevdata];
+    })
+  }
+
+  const deleteHandler = (deltetedItemId) => {
+    // console.log(deltetedItemId);
+    setData(prevData => {
+      return prevData.filter(item => {
+        // console.log(item.id)
+       return  item.id !== deltetedItemId
+      })
+    })
+
+};
+
+
+  const foodData = data.filter((item) => { return item.cat === 'food' });
+  const eleetronicData = data.filter((item) => { return item.cat === "Electronics" });
+  const skinData = data.filter((item) => {
+    return item.cat === "SkinCare";
+  });
+  
+  
+
+  return (
+    <React.Fragment>
+      <Form addItem={addDataHandler} />
+      <List ondelete={deleteHandler} category={'Food'} listItems={foodData} />
+      <List ondelete={deleteHandler} category={'Electronics'} listItems={eleetronicData} />
+      <List ondelete={deleteHandler} category={'SkinCare'} listItems={skinData} />
+      
+    </React.Fragment>
+  );
+}
+
+export default App;
